b0e5c74473fb132ad8a64d56a8e2af94ef6cce40
cgranados16 <cgranados@ltvco.com>
2023-10-09 12:22:13 -0600

    Fix icons path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "@fontsource/lato";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 import "@shoelace-style/shoelace/dist/themes/light.styles";
-setBasePath("dist/shoelace");
+setBasePath("dist/");
 import { BpButton } from "./Button/Button";
 export { BpButton };
